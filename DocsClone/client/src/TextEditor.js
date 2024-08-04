b29497120880ee7e36901f329c9d1748de7f4cd5
Lucy <lucyziwenzhou@gmail.com>
2024-08-04 00:54:46 -0700

    setup the text editor

--- DocsClone/client/src/TextEditor.js
+++ DocsClone/client/src/TextEditor.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useCallback} from 'react'
+import Quill from "quill";
+import "quill/dist/quill.snow.css"
 
 export default function TextEditor() {
-  return (
-    <div>
-        TextEditor
-    </div>
-  )
+    const wrapperRef = useCallback((wrapper) => { // creates a text editor inside a container using quill
+        if(wrapper==null) return // make sure we have a wrapper
+
+        wrapper.innerHTML = "" // cleans the page
+        const editor = document.createElement('div')
+        wrapper.append(editor) // adds the tool bar to the editor 
+        new Quill(editor, {theme: "snow"})
+    }, [])
+    return <div id="container" ref={wrapperRef}></div>
 }
