c98aa337610514385c7340f457a298ff1a5925da
Lucy <lucyziwenzhou@gmail.com>
2024-08-12 23:45:23 -0700

    video 19 - create EventHandler class and set up trigger point

---
+++ 2DGameTut/src/main/java/lza/dgametut/EventHandler.java
@@ -0,0 +1,22 @@
+package lza.dgametut;
+
+import java.awt.*;
+
+public class EventHandler {
+    GamePanel gp;
+    Rectangle eventRect;
+    int eventRectDefaultX, eventRectDefaultY;
+
+    public EventHandler(GamePanel gp){
+        this.gp = gp;
+
+        // set the event trigger point at the middle of the tile
+        eventRect = new Rectangle();
+        eventRect.x = 23;
+        eventRect.y = 23;
+        eventRect.width = 2;
+        eventRect.height = 2;
+        eventRectDefaultX = eventRect.x;
+        eventRectDefaultY = eventRect.y;
+    }
+}
