29c4bf6e6c119a5ee5985da51981d7acbe7bc5b2
Lucy <lucyziwenzhou@gmail.com>
2024-08-13 22:56:52 -0700

    video 19 - check for event collision

--- 2DGameTut/src/main/java/lza/dgametut/EventHandler.java
+++ 2DGameTut/src/main/java/lza/dgametut/EventHandler.java
@@ -19,4 +19,38 @@ public class EventHandler {
         eventRectDefaultX = eventRect.x;
         eventRectDefaultY = eventRect.y;
     }
+
+    public void checkEvent(){
+        if(hit(27, 16, "right"))
+            damagePit(gp.dialogueState);
+
+    }
+
+    public boolean hit(int eventCol, int eventRow, String reqDirection){
+        // checks for event collision
+        boolean hit = false;
+
+        gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
+        gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
+        eventRect.x = eventCol*gp.tileSize + eventRect.x;
+        eventRect.y = eventRow*gp.tileSize + eventRect.y;
+
+        if(gp.player.solidArea.intersects(eventRect)){
+            if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any"))
+                hit = true;
+        }
+
+        gp.player.solidArea.x = gp.player.solidAreaDefaultX;
+        gp.player.solidArea.y = gp.player.solidAreaDefaultY;
+        eventRect.x = eventRectDefaultX;
+        eventRect.y = eventRectDefaultY;
+
+        return hit;
+    }
+
+    public void damagePit(int gameState){
+        gp.gameState = gameState;
+        gp.ui.currentDialogue = "You fall into a pit!";
+        gp.player.life -= 1;
+    }
 }
