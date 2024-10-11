48c0ec45d237dc600ce4973986108b757cee3184
Lucy <lucyziwenzhou@gmail.com>
2024-10-10 19:39:46 -0700

    set up the console ASCII printer

--- ASCIIShooter/Source.cpp
+++ ASCIIShooter/Source.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+#include <windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
 int main(){
-    for (int i = 0; i< 10000000000; i++) cout << "Hello" << endl;
+    // Create Screen Buffer
+    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    while(1){
+        screen[nScreenWidth * nScreenHeight - 1] = '/0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0,0}, &dwBytesWritten); // writes to top right corner (console doesn't scroll down)
+    }
+    screen[nScreenWidth * nScreenHeight - 1] = '/0';
+    WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0,0}, &dwBytesWritten); // writes to top right corner (console doesn't scroll down)
 
     return 0;
 }
